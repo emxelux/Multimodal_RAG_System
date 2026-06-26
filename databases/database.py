e2dc948e8f4d07660354507305b47c211b1cd6a2
unknown <emmanuelolanrewaju996@gmail.com>
2026-06-26 11:07:58 +0100

    Fixed dead database

--- databases/database.py
+++ databases/database.py
@@ -14,7 +14,16 @@ load_dotenv()
 SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
 
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL,
+    # 1. Checks if connection is alive before executing a query (pings the DB)
+    pool_pre_ping=True, 
+    # 2. Automatically recycles connections older than 1 hour (3600 seconds)
+    pool_recycle=3600,  
+    # 3. Keeps a stable pool of connections
+    pool_size=10,
+    max_overflow=20
+)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
